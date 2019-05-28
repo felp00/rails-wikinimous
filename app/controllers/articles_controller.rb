a806a1ad9796df3fe9a4ca0d9ba90e9448f969a3
Felipe Rocha <felipewp200@gmail.com>
2019-05-28 16:58:24 -0300

    wikinimous

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,45 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @articles = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
